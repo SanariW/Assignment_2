8b5e34be338456237320ed6f989865bb4554dd25
SanariW <135373691+SanariW@users.noreply.github.com>
2023-06-14 08:58:46 -0400

    Assignment_2.R

--- Assignment_2.R
+++ Assignment_2.R
@@ -7,8 +7,8 @@
 user_input <- readline(prompt = "Please enter a three-digit, positive number: ")
 
 # Checking if the user input is numeric, three digits, and positive. If not, print an error message and quit.
-# If the user input meets all three criteria, then can determine if input is narcissistic.
-# Print appropriate message if user input is a narcissistic number or not.
+# If the user input meets all three criteria, then can determine if input is Narcissistic.
+# Print appropriate message if user input is a Narcissistic number or not.
 # ASSUMPTION: 0 is not a positive number.
 
 if(is.numeric(as.numeric(user_input)) & # This is to check if user input is numeric.
@@ -17,15 +17,15 @@ if(is.numeric(as.numeric(user_input)) & # This is to check if user input is nume
    (as.numeric(user_input)) > 0) { # This is to check if the user input is a positive number.
      value <- as.integer(user_input) # Ensure user input is integer and not a character.
 
-       splitdigits <- as.numeric(strsplit(user_input, "") [[1]]) # Splitting given user input into a list of values.
+       splitdigits <- as.numeric(strsplit(user_input, "") [[1]]) # Splitting given user input into a list of digits.
 
        sum_of_cubes <- sum(splitdigits ^ 3) # Determining the sum of cubes of each digit of the user input after the user input is split into digits.
                                             # For example, if user input is "153", each digit ("1", "5", "3") will be cubed and summed.
 
        if((as.numeric(user_input)) == (sum_of_cubes)) { # Determining if sum_of_cubes is equal to user_input to see if it is a Narcissistic number.
-         print(paste(user_input, "is a Narcissistic number!"))
+         print(paste(user_input, "is a Narcissistic number! Oh no, the number is a little self-centered!"))
        } else {
-         print(paste(user_input, "is not a Narcissistic number!"))
+         print(paste(user_input, "is not a Narcissistic number! Yay, the number is so emapthic!"))
        }
      } else {
        print(paste(user_input, "is an invalid entry. Entry must be a positive, three-digit number. Terminating..."))
